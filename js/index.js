3c28293f4e06dc9b9bd789a5f25462b1867d18ba
EdwardZack <edwardzack19@gmail.com>
2020-03-28 17:37:26 -0500

    Calculadora v3
    reducir las teclas y volverlas a su tamaño

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
+//declaramos el objeto calculadora con todas sus variables y funciones.
 var Calculadora = {
   init: function(){
-    
+    this.asignarEventosTeclas('tecla')
+  },
+//función que asigna el cambio de tamaño de las teclas al evento onclick.
+  asignarEventosTeclas: function(selector){
+    var teclas = document.getElementsByClassName(selector);
+    for (var i=0; i<teclas.length; i++){
+      teclas[i].onmousedown = this.eventoTeclaLarge;
+      teclas[i].onmouseup = this.eventoTeclaSmall;
+    }
+  },
+//función para el cambio de tamaño de las teclas.
+  eventoTeclaLarge: function(event){
+    var tecla = event.target;
+    tecla.style.padding = "0.1rem";
+  },
+  eventoTeclaSmall: function(event){
+    var tecla = event.target;
+    tecla.style.padding = "0";
   }
 }
+
+//ejecutamos la función inicializadora del objeto calculadoraFondo.
+Calculadora.init()
